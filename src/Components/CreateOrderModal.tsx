8d0af7e54ead92d13e4dd26862c9897cccbb50d7
Samuel Wemimo <wemsamuel@gmail.com>
2024-03-01 16:36:51 -0700

    fix: fixing model size

--- src/Components/CreateOrderModal.tsx
+++ src/Components/CreateOrderModal.tsx
@@ -18,7 +18,7 @@ const style = {
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: 400,
+  width: 250,
   bgcolor: 'background.paper',
   boxShadow: 24,
   p: 4,
@@ -71,84 +71,80 @@ const CreateOrderModal = ({
   };
 
   return (
-    <div>
-      <Modal
-        open={open}
-        onClose={onClose}
-        aria-labelledby="modal-title"
-        aria-describedby="modal-description"
-      >
-        <Box sx={style}>
-          <Typography id="modal-title" variant="h6" component="h3">
-            Create New Order
-          </Typography>
-          <FormControl fullWidth sx={{ mt: 2 }}>
-            <TextField
-              label="Full Name"
-              name="customerName"
-              value={formData.customerName}
-              onChange={handleTextFieldChange}
-            />
-          </FormControl>
-          <FormControl fullWidth sx={{ mt: 2 }}>
-            <TextField
-              label="Created By"
-              name="createdByUserName"
-              value={formData.createdByUserName}
-              onChange={handleTextFieldChange}
-            />
-          </FormControl>
-          <FormControl fullWidth sx={{ mt: 2 }}>
-            <InputLabel id="order-type-label">Order Type</InputLabel>
-            <Select
-              labelId="order-type-label"
-              name="order-type-select"
-              value={formData.orderType}
-              onChange={handleOrderValuesChange}
-              input={<OutlinedInput label="Order Type" />}
-            >
-              <MenuItem value={OrderType.PurchaseOrder}>
-                {OrderType.PurchaseOrder}
-              </MenuItem>
-              <MenuItem value={OrderType.ReturnOrder}>
-                {OrderType.ReturnOrder}
-              </MenuItem>
-              <MenuItem value={OrderType.SaleOrder}>
-                {OrderType.SaleOrder}
-              </MenuItem>
-              <MenuItem value={OrderType.Standard}>
-                {OrderType.Standard}
-              </MenuItem>
-              <MenuItem value={OrderType.TransferOrder}>
-                {OrderType.TransferOrder}
-              </MenuItem>
-            </Select>
-          </FormControl>
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={handleOrderSubmit}
-            sx={{ mt: 2 }}
+    <Modal
+      open={open}
+      onClose={onClose}
+      aria-labelledby="modal-title"
+      aria-describedby="modal-description"
+    >
+      <Box sx={style}>
+        <Typography id="modal-title" variant="h6" component="h3">
+          Create New Order
+        </Typography>
+        <FormControl fullWidth sx={{ mt: 2 }}>
+          <TextField
+            label="Full Name"
+            name="customerName"
+            value={formData.customerName}
+            onChange={handleTextFieldChange}
+          />
+        </FormControl>
+        <FormControl fullWidth sx={{ mt: 2 }}>
+          <TextField
+            label="Created By"
+            name="createdByUserName"
+            value={formData.createdByUserName}
+            onChange={handleTextFieldChange}
+          />
+        </FormControl>
+        <FormControl fullWidth sx={{ mt: 2 }}>
+          <InputLabel id="order-type-label">Order Type</InputLabel>
+          <Select
+            labelId="order-type-label"
+            name="order-type-select"
+            value={formData.orderType}
+            onChange={handleOrderValuesChange}
+            input={<OutlinedInput label="Order Type" />}
           >
-            Submit Order
-          </Button>
-          <Button
-            color="inherit"
-            onClick={() => {
-              onClose();
-              setFormData({
-                customerName: '',
-                createdByUserName: '',
-                orderType: '',
-              });
-            }}
-            sx={{ position: 'absolute', top: 10, right: 10 }}
-          >
-            <CloseIcon />
-          </Button>
-        </Box>
-      </Modal>
-    </div>
+            <MenuItem value={OrderType.PurchaseOrder}>
+              {OrderType.PurchaseOrder}
+            </MenuItem>
+            <MenuItem value={OrderType.ReturnOrder}>
+              {OrderType.ReturnOrder}
+            </MenuItem>
+            <MenuItem value={OrderType.SaleOrder}>
+              {OrderType.SaleOrder}
+            </MenuItem>
+            <MenuItem value={OrderType.Standard}>{OrderType.Standard}</MenuItem>
+            <MenuItem value={OrderType.TransferOrder}>
+              {OrderType.TransferOrder}
+            </MenuItem>
+          </Select>
+        </FormControl>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={handleOrderSubmit}
+          sx={{ mt: 2 }}
+        >
+          Submit Order
+        </Button>
+        <Button
+          color="inherit"
+          onClick={() => {
+            onClose();
+            setFormData({
+              customerName: '',
+              createdByUserName: '',
+              orderType: '',
+            });
+          }}
+          sx={{ position: 'absolute', top: 10, right: 10 }}
+        >
+          <CloseIcon />
+        </Button>
+      </Box>
+    </Modal>
   );
 };
 
