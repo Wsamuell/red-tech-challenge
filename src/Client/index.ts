8287ac6e3118d248216d3dbd81de9e015fa53805
Samuel Wemimo <wemsamuel@gmail.com>
2024-03-03 14:30:52 -0700

    wip: forgot to implement getOrderByType

--- src/Client/index.ts
+++ src/Client/index.ts
@@ -10,21 +10,27 @@ const fetchData = async (
   method: string,
   body?: NewOrder | Order | string[]
 ) => {
+  const requestOptions: RequestInit = {
+    method,
+    headers: {
+      'Content-Type': 'application/json',
+      ApiKey: REACT_APP_API_KEY!,
+    },
+  };
+
+  if (body) {
+    requestOptions.body = JSON.stringify(body);
+  }
+
   try {
-    const response = await fetch(`${REACT_APP_URL}${url}`, {
-      method,
-      headers: {
-        'Content-Type': 'application/json',
-        ApiKey: REACT_APP_API_KEY!,
-      },
-      body: body ? JSON.stringify(body) : undefined,
-    });
+    const response = await fetch(`${REACT_APP_URL}${url}`, requestOptions);
 
     if (!response.ok) {
       throw new Error(
         `Failed to fetch data. Status: ${response.status}: ${response.statusText}`
       );
     }
+
     return await response.json();
   } catch (error: any) {
     throw new Error(`Failed to fetch data: ${error.message}`);
@@ -32,76 +38,30 @@ const fetchData = async (
 };
 
 export const fetchAllOrders = async () => {
-  try {
-    const response = await fetch(`${REACT_APP_URL}/api/Orders`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        ApiKey: REACT_APP_API_KEY!,
-      },
-    });
-
-    if (!response.ok) {
-      throw new Error(
-        `Failed to fetch data. Status: ${response.status}: ${response.statusText}`
-      );
-    }
-    return await response.json();
-  } catch (error: any) {
-    throw new Error(`Failed to fetch data: ${error.message}`);
-  }
+  return fetchData('/api/Orders', 'GET');
 };
 
 export const addNewOrder = async (order: NewOrder) => {
-  try {
-    const response = await fetch(`${REACT_APP_URL}/api/Orders/`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        ApiKey: REACT_APP_API_KEY!,
-      },
-      body: JSON.stringify(order),
-    });
-
-    if (!response.ok) {
-      throw new Error(
-        `Failed to fetch data. Status: ${response.status}: ${response.statusText}`
-      );
-    }
-    return response.json();
-  } catch (error: any) {
-    throw new Error(`Failed to fetch data: ${error.message}`);
-  }
+  return fetchData('/api/Orders/', 'POST', order);
 };
 
 export const updateOrder = async (order: Order) => {
   return fetchData('/api/Orders', 'PUT', order);
 };
 
-export const getOrderByType = async (type: OrderType) => {
-  try {
-    const response = await fetch(
-      `${REACT_APP_URL}/api/Orders/ByType?orderType=${type}`,
-      {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          ApiKey: REACT_APP_API_KEY!,
-        },
-      }
-    );
+// Ran into a dilema
+//  - we wont be able to search for multiple types if i use this type
+//  - we wont have access to all the orders to search by order id
+// ultimately decided not to use this values and just use what we have in state to create search
+// this decision led to creating a second order type called filteredOrder to keep a list of orders that are filtered to display so we dont have to re run fetch multiple times
 
-    if (!response.ok) {
-      throw new Error(
-        `Failed to fetch data. Status: ${response.status}: ${response.statusText}`
-      );
-    }
-    return await response.json();
-  } catch (error: any) {
-    throw new Error(`Failed to fetch data: ${error.message}`);
-  }
+// although i know this decision wont be right in a large application, i am willing to talk more about it during a follow up interview
+
+export const getOrderByType = async (type: OrderType) => {
+  return fetchData(`/api/Orders/ByType?orderType=${type}`, 'GET');
 };
 
+// delete doesnt return anything so it makes sense to tear out this functionality
 export const deleteOrder = async (orderId: string[]) => {
   try {
     const response = await fetch(`${REACT_APP_URL}/api/Orders/Delete`, {
